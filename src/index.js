2d60477f9368dcaa0b37c21e599bc27ffff4d8c8
josue2985 <jbrito@opstrax.com>
2019-10-16 00:01:13 -0500

    video implementation

--- src/index.js
+++ src/index.js
@@ -1,31 +1,52 @@
 /*import { bro } from './bro';*/
 import './styles/main.scss';
 
-TweenLite.defaultEase = Linear.easeNone;
-var controller = new ScrollMagic.Controller();
-var tl = new TimelineMax();
-tl.staggerFrom(".box", 1.5, {
-  scale: 0,
-  cycle: {
-    y: [-50, 50]
-  },
-  stagger: {
-    from: "center",
-    amount: 0.75
-  }
+
+const video = document.getElementById('video');
+const long = document.getElementById('long');
+let scrollpos = 0;
+let lastpos;
+const controller = new ScrollMagic.Controller();
+const scene = new ScrollMagic.Scene({
+  triggerElement: long,
+  triggerHook: "onEnter"
 });
+const startScrollAnimation = () => {
+  scene
+    .addTo(controller)
+    .duration(long.clientHeight)
+    .on("progress", (e) => {
+      scrollpos = e.progress;
+    });
 
-var scene = new ScrollMagic.Scene({
-  triggerElement: "#stage",
-  duration: "30%",
-  triggerHook: 0.25
-})
-  .addIndicators({
-    name: "Box Timeline",
-    colorTrigger: "white",
-    colorStart: "white",
-    colorEnd: "white"
-  })
-  .setTween(tl)
-  .addTo(controller);
+  setInterval(() => {
+    if (lastpos === scrollpos) return;
+    requestAnimationFrame(() => {
+      video.currentTime = video.duration * scrollpos;
+      video.pause();
+      lastpos = scrollpos;
+      // console.log(video.currentTime, scrollpos);
+    });
+  }, 50);
+};
 
+const preloadVideo = (v, callback) => {
+  const ready = () => {
+    v.removeEventListener('canplaythrough', ready);
+
+    video.pause();
+    var i = setInterval(function() {
+      if (v.readyState > 3) {
+        clearInterval(i);
+        video.currentTime = 0;
+        callback();
+      }
+    }, 50);
+  };
+  v.addEventListener('canplaythrough', ready, false);
+  v.play();
+};
+
+preloadVideo(video, startScrollAnimation);
+
+// startScrollAnimation();
